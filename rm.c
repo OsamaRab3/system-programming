3ef2022ac72e2436f3d5c412f01ef597c46933d7
osama <osrab3@gmail.com>
2024-06-28 12:50:46 +0300

    implement update rm command line

--- rm.c
+++ rm.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
+/*
+    Simulates the behavior of the 'rm' command in Linux
+    rm file.txt
+    If the file does not exist show  error 
+    if found delete it 
+*/
 int main(int argc,char *argv[])
 {
     if (argc < 2)
     {
         printf("Usage: %s file\n", argv[0]);
+        return 1;
+    }
+    if (remove(argv[1]) != 0)
+    {
+        perror("rm");
         return 1;
     }
     remove(argv[1]);
