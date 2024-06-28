9de5bd045e63e1dd492b7436f9e1d37e7071eb36
osama <osrab3@gmail.com>
2024-06-28 14:35:40 +0300

    implement pwd command line

---
+++ pwd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+    Simulates the behavior of the 'pwd' command in Linux
+    pwd
+    print name of current/working directory.
+*/
+int main()
+{
+    char working_directory[1024];
+    if (getcwd(file_path, sizeof(working_directory)) != NULL)
+        printf("%s\n", working_directory);
+    else{
+    perror("getcwd() erorr ";)
+    return 1;
+    }
+
+    return 0;
+}
