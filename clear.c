4c41d7754f108bede50b448fab7c228f3a0385c5
osama <osrab3@gmail.com>
2024-07-01 18:41:54 +0300

    implement clear command line

---
+++ clear.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Simulates the behavior of the 'clear' command in Linux
+    clear => Clear terminal
+
+*/
+
+int main()
+{
+    system("clear"); 
+    return 0;
+}
